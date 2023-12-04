8af1455010d4185c6d2a360e50fa9e73545cbfa1
RogerMeroDev <rog3r.mero@gmail.com>
2023-12-03 20:45:08 -0500

    fondode pagina

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />        
-      </Routes>
+    
+    <Router>
+      <div className="fondoConDesenfoque" /> {/* Fondo con gradiente y desenfoque */}
+      <div className="contenidoPrincipal">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />        
+        </Routes>
+      </div>
     </Router>
+    
   );
 }
 
